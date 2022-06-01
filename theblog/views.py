27c49ed81b5207b3afc772bb24494fea94a83e16
Steven Kato <33441936+stevensnowden3@users.noreply.github.com>
2022-06-01 09:21:35 +0300

    added category in dropdown
    fully works

--- theblog/views.py
+++ theblog/views.py
@@ -14,10 +14,13 @@ class HomeView(ListView):
     template_name = 'home.html'
     # ordering = ['-id']
     ordering = ['-created_at']
+    
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView,self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
-def CategoryView(request,cats):
-    category_posts = Post.objects.filter(category=cats)
-    return render(request, 'category.html', {'cats':cats.title(), 'category_posts':category_posts})
 
 class ArticlaDetailView(DetailView):
     model = Post
@@ -35,7 +38,7 @@ class AddPostView(CreateView):
 class AddCategoryView(CreateView):
     model = Category
     template_name = 'add_category.html'
-    fields =  '__all__'
+    fields = '__all__'
 
 
 class UpdatePostView(UpdateView):
@@ -48,7 +51,8 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
-    
 
 
-
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'category.html', {'cats': cats.title(), 'category_posts': category_posts})
