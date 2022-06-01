457bdcee24a84d435ed2828e1c5939e0b3e7db6b
Steven Kato <33441936+stevensnowden3@users.noreply.github.com>
2022-06-01 19:20:58 +0300

    fixed add post bug, hide author from add post

--- theblog/forms.py
+++ theblog/forms.py
@@ -13,7 +13,9 @@ class   PostForm(forms.ModelForm):
 
         widgets = {
             'title' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'author' : forms.Select(attrs={'class' : 'form-control'}),
+            # there is java script for this
+            'author' : forms.TextInput(attrs={'class' : 'form-control','value':'', 'id':'admin', 'type':'hidden'}),
+            # 'author' : forms.Select(attrs={'class' : 'form-control'}),
             'category' : forms.Select(choices = choice_list , attrs={'class' : 'form-control'}),
             'body' : forms.Textarea(attrs={'class' : 'form-control'}),
             'created_at' : forms.TextInput(attrs={'class' : 'form-control'}),
