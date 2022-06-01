cba4d9916b7ec22937c5c7f09d503eb37ce14b2f
Steven Kato <33441936+stevensnowden3@users.noreply.github.com>
2022-06-02 02:19:15 +0300

    user can now update their profile

--- members/urls.py
+++ members/urls.py
@@ -1,10 +1,14 @@
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import UserRegistrationView, UserEditView
+from .views import UserRegistrationView, UserEditView,PasswordsChangeView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name="register"),
     path('edit_profile/', UserEditView.as_view(), name="edit_profile"),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html')),
+    path('password_success/',views.password_success, name="password_success"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- members/views.py
+++ members/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 # from django.contrib.auth.forms import UserCreationForm, UserChangeForm, used previously as default forms
+from django.contrib.auth.forms import PasswordChangeForm
 from .forms import SingUpForm,EditProfileForm
+from django.contrib.auth.views import PasswordChangeView
+
 
 class UserRegistrationView(generic.CreateView):
     form_class = SingUpForm
@@ -18,6 +21,16 @@ class UserEditView(generic.UpdateView):
 
     def get_object(self,):
         return self.request.user
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('password_success')
+
+def password_success(request):
+    return render(request, 'registration/password_success.html')
+
+
+        
 
 
 
