ec476883e67ee12a0a78322fa2f17520ff66621f
Steven Kato <33441936+stevensnowden3@users.noreply.github.com>
2022-06-02 00:12:05 +0300

    bootstrapfied register form!!

--- members/views.py
+++ members/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SingUpForm
 
 class UserRegistrationView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SingUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
