c776ec56c611f5ffc19a4645d30bac5c0108bdd5
Steven Kato <33441936+stevensnowden3@users.noreply.github.com>
2022-06-03 14:23:26 +0300

    fully working app no bugs locally

--- CodeBlogProject/settings.py
+++ CodeBlogProject/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
-import django_heroku
-from decouple  import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l0$_)3@t!2a=9#g&(@(!_f5438$s=&rpiq8&e+r4rrv83ykg*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['simpleblogpost.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -58,8 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoise.Middleware',
-
 ]
 
 ROOT_URLCONF = 'CodeBlogProject.urls'
@@ -136,8 +132,6 @@ MEDIA_ROOT= "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 
 # Default primary key field type
@@ -147,5 +141,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL  = 'home'
 LOGOUT_REDIRECT_URL  = 'home'
-
-django_heroku.settings(locals())
