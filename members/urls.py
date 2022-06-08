434811a3d91f9cba813b2c1793fcd75924f77d09
Steven Kato <33441936+stevensnowden3@users.noreply.github.com>
2022-06-08 14:56:46 +0300

    more cooler

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import UserRegistrationView, UserEditView,PasswordsChangeView, ShowProfileView,EditProfilePageView,CreateProfilePageView
+from .views import UserRegistrationView, UserEditView, PasswordsChangeView, ShowProfileView, EditProfilePageView, CreateProfilePageView
 # from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,10 +9,17 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name="register"),
     path('edit_profile/', UserEditView.as_view(), name="edit_profile"),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html')),
-    path('password_success/',views.password_success, name="password_success"),
+    path('password_success/', views.password_success, name="password_success"),
     path('<int:pk>/profile', ShowProfileView.as_view(), name="user_profile"),
-    path('<int:pk>/edit_profile_page', EditProfilePageView.as_view(), name="edit_profile_page"),
-    path('create_profile_page/', CreateProfilePageView.as_view(), name="create_user_profile_page"),
+    path('<int:pk>/edit_profile_page',
+         EditProfilePageView.as_view(), name="edit_profile_page"),
+    path('create_profile_page/', CreateProfilePageView.as_view(),
+         name="create_user_profile_page"),
+
+    path('passwordw/', views.password_su, name="password_su"),
+
+
+
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- members/views.py
+++ members/views.py
@@ -2,12 +2,12 @@
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views import generic
-from django.views.generic  import DetailView,CreateView
-# from django.contrib.auth.forms import UserCreationForm, UserChangeForm, used previously as default forms
-from django.contrib.auth.forms import PasswordChangeForm
+from django.views.generic import DetailView, CreateView
+# from django.contrib.auth.forms import UserCreationForm, UserChangeForm, used previously as default for
+
 
 from theblog.models import Profile
-from .forms import SingUpForm,EditProfileForm, PasswordChangingForm,ProfilePageForm
+from .forms import SingUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm
 from django.contrib.auth.views import PasswordChangeView
 
 
@@ -18,10 +18,10 @@ class CreateProfilePageView(CreateView):
     # fields = '__all__'
     # success_url = reverse_lazy('login')
 # making user id avaible when calling page..... replaced javascript code
-    def form_valid(self,form):
+
+    def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-        
 
 
 class UserRegistrationView(generic.CreateView):
@@ -34,19 +34,17 @@ class EditProfilePageView(generic.UpdateView):
     model = Profile
     template_name = 'registration/edit_profile_page.html'
     success_url = reverse_lazy('home')
-    fields = ['bio','profile_pic','facebook','twitter','website']
+    fields = ['bio', 'profile_pic', 'facebook', 'twitter', 'website']
 
 
-   
-
-        
 class ShowProfileView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
 
     def get_context_data(self, *args, **kwargs):
         user = Profile.objects.all()
-        context = super(ShowProfileView,self).get_context_data(*args, **kwargs)
+        context = super(ShowProfileView, self).get_context_data(
+            *args, **kwargs)
 
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
 
@@ -62,17 +60,16 @@ class UserEditView(generic.UpdateView):
     def get_object(self):
         return self.request.user
 
+
 class PasswordsChangeView(PasswordChangeView):
     # form_class = PasswordChangeForm //former
     form_class = PasswordChangingForm
     success_url = reverse_lazy('password_success')
 
+
 def password_success(request):
     return render(request, 'registration/password_success.html')
 
 
-        
-
-
-
-
+def password_su(request):
+    return render(request, 'registration/password_su.html')
